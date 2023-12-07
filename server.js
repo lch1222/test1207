c450ce1cb94f7205b5096ba71d996d414132c575
Noah <a3865252@gmail.com>
2023-12-07 23:55:17 +0800

    a API

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
-const jsonServer = require('json-server')
-const auth = require('json-server-auth')
+// const jsonServer = require('json-server')
+// const auth = require('json-server-auth')
 
-const app = jsonServer.create()
-const router = jsonServer.router('login-post.json')
+// const app = jsonServer.create()
+// const router = jsonServer.router('login-post.json')
 
-// /!\ Bind the router db to the app
-app.db = router.db
+// // /!\ Bind the router db to the app
+// app.db = router.db
 
-// You must apply the auth middleware before the router
-app.use(auth)
-app.use(router)
-app.listen(3000)
+// // You must apply the auth middleware before the router
+// app.use(auth)
+// app.use(router)
+// app.listen(3000)
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+
+
+const server = jsonServer.create();
+const router = jsonServer.router("login-post.json"); // 這裡是你的數據庫文件
+const middlewares = jsonServer.defaults();
+
+server.db = router.db;
+
+server.use(middlewares);
+server.use(auth); // 添加 auth 支持
+server.use(router);
+
+server.listen(3000, () => {
+  console.log("JSON Server is running on port 3000");
+});
